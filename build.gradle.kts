c23be2001cc265b1b05837ee996c20cff6fed479
ethernet389 <ethernet389@gmail.com>
2023-09-24 17:59:43 +0800

    No integration with JitpackIO

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.10"
-    id("java") apply true
-    id("maven-publish") apply true
     application
 }
 
@@ -14,7 +12,6 @@ repositories {
 }
 
 dependencies {
-
     //Jama (Java Matrix)
     val jama_version = "1.0.3"
     implementation("gov.nist.math:jama:$jama_version")
@@ -36,23 +33,4 @@ kotlin {
 
 application {
     mainClass.set("math.MainKt")
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "cm.github.ethernet389"
-            artifactId = "MAI-Core"
-            version = "1.0.0 "
-
-            afterEvaluate {
-                from(components["java"])
-            }
-        }
-    }
 }
